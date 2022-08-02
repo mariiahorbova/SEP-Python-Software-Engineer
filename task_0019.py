ef650d69d2316d3f75a180f00e3264b0c72504df
Mariia Horbova <44654425+mariiahorbova@users.noreply.github.com>
2022-08-03 02:35:38 +0300

    tasks 14-21

---
+++ task_0019.py
@@ -0,0 +1,8 @@
+# 19. Write a Python program to print “Python Basic Exercises" 9 times.
+
+def print_9_times(string):
+    for i in range(9):
+        print(string)
+        
+
+print_9_times("Python Basic Exercises")
---
+++ task_0021.py
@@ -0,0 +1,17 @@
+# 21. Write a Python program to print 34 upto 41. 
+# Sample Output:
+# 34
+# 35
+# 36
+# 37
+# 38
+# 39
+# 40
+# 41
+# 42
+
+def print_range():
+    for i in range(34, 42):
+        print(i)
+        
+print_range()
