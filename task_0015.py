e8c9bee61b041cb47d5601e0093031a5d34ee19f
Mariia Horbova <44654425+mariiahorbova@users.noreply.github.com>
2022-08-03 14:34:18 +0300

    Revert "Merge pull request #1 from mariiahorbova/data_types_etc"
    This reverts commit cdb257da7bd553aeda669145cf57700e5a7a8dfd, reversing
    changes made to 8447b8a932cb838bd4cce408f078063cec3fe736.

--- task_0015.py
+++
@@ -1,9 +0,0 @@
-# 15. Write a Python program to check two integers and return true if one of them is 20 otherwise return their sum.
-
-def check_20(num1, num2):
-    return "true" if (num1 == 20 or num2 == 20) else num1 + num2
-
-print(check_20(20, 19))
-print(check_20(17, 19))
-print(check_20(10, 14))
-print(check_20(3, 20))
--- task_0019.py
+++
@@ -1,8 +0,0 @@
-# 19. Write a Python program to print “Python Basic Exercises" 9 times.
-
-def print_9_times(string):
-    for i in range(9):
-        print(string)
-        
-
-print_9_times("Python Basic Exercises")
